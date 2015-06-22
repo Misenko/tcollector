f5f7f4112af525c46f0b7300b320224ce2b4e60a
Michal Kimle <kimle.michal@gmail.com>
2015-06-23 00:00:31 +0200

    added loading of memory and network traffic data

--- collectors/0/libvirt.py
+++ collectors/0/libvirt.py
@@ -16,8 +16,11 @@
 import sys
 import time
 import libvirt
+import os
+import collections
 
 from collectors.lib import utils
+from bs4 import BeautifulSoup
 
 INTERVAL = 15  # seconds
 
@@ -45,15 +48,88 @@ PID_DIR = "/var/run/libvirt/qemu"
 
 LIBVIRT_URI = "qemu:///system"
 
+class LibvirtCollectorError(Exception):
+    pass
+
 def main():
     conn = libvirt.openReadOnly(LIBVIRT_URI)
     if conn is None:
         utils.err("Failed to open connection to the hypervisor")
-        return
+        return 13 # tells tcollector not to restart this collector after failure
 
     domains = conn.listAllDomains()
-    vms = []
+    batches = chunker(domains, BATCH_SIZE)
 
+    for batch in batches:
+        vms = {}
+        for domain in batch:
+            try:
+                vm = {}
+                vm["name"] = domain.name()
+                vm[FIELD["memory"]] = get_memory(domain)
+                vm.update(get_network_traffic(domain))
+            except LibvirtCollectorError, err:
+                utils.err(err.message)
+                continue
+
+            vm_pid_file = "%s/%s.pid" % PID_DIR, vm["name"]
+
+            if !os.path.isfile(vm_pid_file):
+                utils.err("PID file for virtual machine %s doesn't exist. Skipping." % vm["name"])
+                continue
+
+            pid = ""
+            try:
+                file = open(vm_pid_file)
+                pid = file.readline()
+            except IOError, err:
+                utils.err("Cannot open PID file for virtual machine %s: %sSkipping." % vm["name"], err.message)
+                continue
+            finally:
+                file.close()
+
+            vms[pid.strip()] = vm
+
+        if !vms:
+            continue # no vms in this batch
+            
+        ordered_vms = collections.OrderedDict(sorted(vms.items)))
+        pids = ','.join(ordered_vms.keys())
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in xrange(0, len(seq), size))
+
+def get_memory(domain):
+    if domain.isActive() == 1:
+        mem = domain.memoryStats()
+        return max([mem["actual"], mem["rss"]])
+    else:
+        xml = BeautifulSoup(domain.XMLDesc())
+        try:
+            mem = int(xml.memory.getText())
+        except ValueError:
+            raise LibvirtCollectorError("Cannot read memory for domain %s. Skipping" % domain.name())
+
+    return mem
+
+def get_network_traffic(domain):
+    xml = BeautifulSoup(domain.XMLDesc())
+    interfaces = xml.findAll("interface")
+    netrx = 0
+    nettx = 0
+    for interface in interfaces:
+        target = interface.target
+        if target != None && target.has_attr("dev"):
+            try:
+                netrx += domain.interfaceStats(interface.target["dev"])[0] # netrx
+                nettx += domain.interfaceStats(interface.target["dev"])[4] # nettx
+            except libvirt.libvirtError:
+                raise LibvirtCollectorError("Cannot read interface statistics for domain %s. Skipping" % domain.name())
+        else:
+            raise LibvirtCollectorError("Cannot read interface name for domain %s. Skipping" % domain.name())
+
+
+    return {FIELDS["netrx"] : netrx, FIELDS["nettx"] : nettx}
 
 if __name__ == '__main__':
     sys.exit(main())
