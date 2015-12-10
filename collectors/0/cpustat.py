8581e2ddb0c2da306010a7bf07674126aa6caccf
Michal Kimle <kimle.michal@gmail.com>
2015-12-10 08:43:25 +0100

    added more metrices for cpustat controller

--- collectors/0/cpustat.py
+++ collectors/0/cpustat.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 #
-# libvirt_vm.py -- a virtual machine data collector for tcollector/OpenTSDB
+# cpustat.py -- cpu data collector for tcollector/OpenTSDB
 # Copyright (C) 2015  Michal Kimle
 #
 # This program is free software: you can redistribute it and/or modify it
@@ -15,29 +15,74 @@
 
 
 # Metrics this collector generates:
-# cpustat.cpu.count - number of CPU
+# cpustat.count - number of CPUs
+# cpustat.cpu.mhz - CPU's MHz
+# cpustat.cpu.cores - CPU's number of cores
 
 import sys
 import time
+import re
 import multiprocessing
 
 from collectors.lib import utils
+from collectors.lib.cpustat_errors import CpustatProcessingError
+
+# gracefully deals with import errors
+try:
+    import numpy.distutils.cpuinfo
+except ImportError:
+    numpy = None
 
 INTERVAL = 15  # seconds
 
 METRIC_PREFIX = "cpustat."
 
-FIELDS = {"cpu_count": "%scpu.count" % METRIC_PREFIX}
+FIELDS = {"cpu_count": "%scount" % METRIC_PREFIX,
+          "cpu_mhz": "%scpu.mhz" % METRIC_PREFIX,
+          "cpu_cores": "%scpu.cores" % METRIC_PREFIX}
+
+TAG_PROCESSOR = "processor"
+TAG_MODEL = "model"
 
 ERROR_CODE_DONT_RETRY = 13  # do not restart this collector after failure
 
 def main():
-    while True:
-        # write cpustat.cpu.count metric
-        print("%s %d %s" % (FIELDS["cpu_count"], int(time.time()), multiprocessing.cpu_count()))
+    try:
+        while True:
+            # write cpustat.cpu.count metric
+            write_cpu_info()
+            write_cpu_count()
 
-        sys.stdout.flush()
-        time.sleep(INTERVAL)
+            sys.stdout.flush()
+            time.sleep(INTERVAL)
+    except CpustatProcessingError as err:
+        utils.err(err.value)
+        return ERROR_CODE_DONT_RETRY
+
+def write_cpu_count():
+    print("%s %d %s" % (FIELDS["cpu_count"], int(time.time()), multiprocessing.cpu_count()))
+
+def write_cpu_info():
+    cpuinfos = numpy.distutils.cpuinfo.cpuinfo.info
+    regex = re.compile(r'[\s@\(\)]+')
+    for cpuinfo in cpuinfos:
+        cpu = {}
+        cpu[FIELDS["cpu_mhz"]] = cpuinfo["cpu MHz"]
+        cpu[FIELDS["cpu_cores"]] = cpuinfo["cpu cores"]
+        cpu[TAG_MODEL] = re.sub(regex, '-', cpuinfo["model name"])
+        cpu[TAG_PROCESSOR] = cpuinfo["processor"]
+
+        print_cpu(cpu)
+
+def print_cpu(cpu):
+    processor = cpu.pop(TAG_PROCESSOR)
+    model = cpu.pop(TAG_MODEL)
+
+    timestamp = int(time.time())
+    for key in cpu.keys():
+        print("%s %d %s %s=%s %s=%s" % (key, timestamp, cpu[key],
+                                        TAG_PROCESSOR, processor,
+                                        TAG_MODEL, model))
 
 if __name__ == '__main__':
     sys.exit(main())
